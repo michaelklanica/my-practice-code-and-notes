78d53f4426ffa605896f4fde5948cd24c8fa7321
Michael Klanica <michael.david.klanica@gmail.com>
2020-09-01 08:39:18 -0500

    adding gitignore

--- js/javascript-review.js
+++ js/javascript-review.js
@@ -1,2 +1,10 @@
 
-document.getElementById('main-heading').innerHTML = "Hello, World";
+
+mapboxgl.accessToken = myToken;
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+
+});
