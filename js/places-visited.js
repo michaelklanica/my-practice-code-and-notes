01203ed80745c6dfb5bc5f89fdf4f90089413433
Michael Klanica <michael.david.klanica@gmail.com>
2020-09-08 12:27:09 -0500

    added places-visited pages for potential app

---
+++ js/places-visited.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = mapboxToken;
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
