ef29f888c4d3a2b07ff5a432669996bc0aa29e24
Michael Klanica <michael.david.klanica@gmail.com>
2020-09-08 12:51:38 -0500

    added geocoding utils and some JS functionality

--- js/places-visited.js
+++ js/places-visited.js
@@ -5,3 +5,19 @@ var map = new mapboxgl.Map({
     center: [-74.5, 40], // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
+
+function geocode(search, token) {
+    var baseUrl = 'https://api.mapbox.com';
+    var endPoint = '/geocoding/v5/mapbox.places/';
+    return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
+        .then(function(res) {
+            return res.json();
+            // to get all the data from the request, comment out the following three lines...
+        }).then(function(data) {
+            return data.features[0].center;
+        });
+}
+
+geocode("San Antonio, TX", mapboxToken).then(function(results) {
+    console.log(results);
+});
